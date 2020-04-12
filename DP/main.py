4648b0dcba4e6048845bbfedf6741ce747b46659
Sandeep-Kothapalli <kothapallisandeep188@gmail.com>
2020-04-12 16:52:37 +0530

    configured coreNLP parsers able to print dependency graphs for sentences in the review text perfectly

--- DP/main.py
+++ DP/main.py
@@ -1,25 +1,44 @@
 import nltk
-from nltk.corpus import treebank
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
+# all codes are run in anaconda with python 3.8.1
+# install nltk using pip
+# uncomment the two lines below for the first time you run the code
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
+# from cgi import escape
 from pprint import pprint
+from nltk.treeprettyprinter import TreePrettyPrinter
+from nltk.parse import CoreNLPParser
+from nltk.parse.corenlp import CoreNLPDependencyParser
+coreNLPurl = 'http://corenlp.run/'
+localServer = 'http://localhost:9000'
+# use local server with desired port number in case you cant use corenlp.run
+dependencyParser = CoreNLPDependencyParser(url=coreNLPurl)
+Parser = CoreNLPParser(url=coreNLPurl)
 def tagging(sentences):
-    tokens = []
-    tagged = []
+    # couldnt get raw_parse_sents to work
+    # res = dependencyParser.raw_parse_sents(sentences)
+    # # list(res)
+    # # print(list(res))
+    # for parsedSentence in res:
+    #     print(item.treepositions() for item in parsedSentence)
+    dependencyRelations = []
     for sentence in sentences:
-        tokens.append(nltk.word_tokenize(sentence))
-    for toks in tokens:
-        tagged.append(nltk.pos_tag(toks))
-    for taggedSentence in tagged:
-        print (taggedSentence)
-    
-    
+        print(sentence)
+        print()
+        res, = dependencyParser.raw_parse(sentence)
+        next(Parser.raw_parse(sentence)).pretty_print()
+        # print(next(Parser.raw_parse(sentence)).height())
+        dependencyRelations.append(res)
+        for governor, dep, dependant in res.triples():
+            print(governor,dep,dependant)
+        print()
+
 def main():
     reviewFile = "review-data.txt"
     reviewData = open(reviewFile)
     text = reviewData.readline();
-    sentences = nltk.sent_tokenize(text)
+    sentences = nltk.sent_tokenize(text);
     tagging(sentences)
-    
+
 if __name__ == "__main__":
     main()
